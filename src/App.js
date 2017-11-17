03ee436dfe2268487d1a3607bcac9551609de2fd
brentkirkland <brentckirkland@gmail.com>
2017-11-17 13:27:20 -0800

    add files. oops

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Senatus</h1>
+        </header>
+        <div className='App-body'>
+          <p className='App-intro'>
+            Welcome to the beginning of Senatus.
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
